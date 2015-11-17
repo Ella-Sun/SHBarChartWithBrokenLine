665de6b696952205ba8026e2d178d1bd18a0fdc9
Sunhong <sunhong@ucfgroup.com>
2015-11-17 16:52:42 +0800

    Initial Commit

---
+++ SHChartWithBrokenLineExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHChartWithBrokenLineExample
+//
+//  Created by SunHong on 15/11/17.
+//  Copyright © 2015年 Sunhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SHChartWithBrokenLineExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SHChartWithBrokenLineExample
+//
+//  Created by SunHong on 15/11/17.
+//  Copyright © 2015年 Sunhong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
